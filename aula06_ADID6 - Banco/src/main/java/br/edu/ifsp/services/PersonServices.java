c4685fb0eb57fb441ea2dbd2a501bb86d7ec09e7
Vinicius dos Santos <viniciussantos1314@gmail.com>
2021-04-26 21:59:29 -0300

    aula 06

---
+++ aula06_ADID6 - Banco/src/main/java/br/edu/ifsp/services/PersonServices.java
@@ -0,0 +1,48 @@
+package br.edu.ifsp.services;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import br.edu.ifsp.exception.ResourceNotFoundException;
+import br.edu.ifsp.model.Person;
+import br.edu.ifsp.repository.PersonRepository;
+
+@Service
+public class PersonServices {
+	
+	@Autowired
+	PersonRepository repository;
+	
+	public Person create(Person person) {
+		//Lógica de persistência na base de dados
+		return repository.save(person);
+	}
+	
+	public Person update(Person person) {
+		Person entity = repository.findById(person.getId())
+				.orElseThrow(() -> new ResourceNotFoundException("Não encontramos registros para este ID"));
+		
+		entity.setFirstName(person.getFirstName());
+		entity.setLastName(person.getLastName());
+		entity.setAdress(person.getAdress());
+		entity.setGender(person.getGender());
+		return repository.save(entity);
+	}
+	
+	public void delete(Long id) {
+		Person entity = repository.findById(id)
+				.orElseThrow(() -> new ResourceNotFoundException("Não encontramos registros para este ID"));
+		repository.delete(entity);
+	}
+	
+	public Person findById(Long id) {
+	return repository.findById(id).orElseThrow(() -> new ResourceNotFoundException("Não encontramos registros para este ID"));
+	}
+	
+	public List<Person> findAll() {
+		return repository.findAll();
+	}
+	
+}
